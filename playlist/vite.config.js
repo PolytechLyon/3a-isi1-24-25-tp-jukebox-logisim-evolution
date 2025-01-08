cff4a32a33ce73bce7afe3a19c6a04f640123b7a
Luka COUTANT <luka.coutant@etu.univ-lyon1.fr>
2025-01-08 14:35:08 +0100

    fix

--- playlist/vite.config.js
+++ playlist/vite.config.js
@@ -8,11 +8,5 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
 })
