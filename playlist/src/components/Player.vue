383a2bebf07d69c32644466d0642038ccb1cb77e
Janet-Doe <lilas.grenier@etu.univ-lyon1.fr>
2025-01-08 16:59:34 +0100

    maybe functional "now playing" message

--- playlist/src/components/Player.vue
+++ playlist/src/components/Player.vue
@@ -1,9 +1,20 @@
 <script setup>
+import { ref } from 'vue';
+import { useGestionPlaylist } from '../composables/gestionPlaylist';
+const { playlist, currentmusic } = useGestionPlaylist();
+const playingMessage = `No music playing.`;
+if (currentmusic != null) {
+    playingMessage = `Now playing: ${currentmusic.value.name}`;
+}
 
 </script>
 
 <template>
     <h2>Player</h2>
+
+    <div class="playing"> {{ playingMessage }} </div>
+
+    <div>
     <div>Choose a song to play</div>
     <fieldset>
         <legend>Playback mode</legend>
@@ -14,10 +25,19 @@
         <input type="radio" id="don-t-repeat" name="mode" value="don-t-repeat" />
         <label for="don-t-repeat">Don't repeat</label>
     </fieldset>
+    </div>
 </template>
 
 <style scoped>
-    fieldset {
-        margin-top: 1rem;
-    }
+fieldset {
+    margin-top: 1rem;
+}
+
+.playing {
+    background-color: lightsteelblue;
+    margin: 1rem;
+    border-radius: 5px;
+    width: auto;
+
+}
 </style>
