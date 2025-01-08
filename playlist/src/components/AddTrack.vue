bf31e3d6631e913a55d460deef0f5eb0871330f3
Luka COUTANT <luka.coutant@etu.univ-lyon1.fr>
2025-01-08 15:18:29 +0100

    bouton uplaod et supprimer

--- playlist/src/components/AddTrack.vue
+++ playlist/src/components/AddTrack.vue
@@ -1,16 +1,43 @@
 <script setup>
+import { ref } from 'vue';
 
+const trackFile = ref(null);
+const isUploadDisabled = ref(true);
+
+const handleMethodChange = (event) => {
+    if (event.target.value === 'upload') {
+        isUploadDisabled.value = !trackFile.value;
+    } else {
+        isUploadDisabled.value = true;
+    }
+}
+
+const handleFileChange = (event) => {
+    trackFile.value = event.target.files[0];
+    if (trackFile.value) {
+        isUploadDisabled.value = false;
+    } else {
+        isUploadDisabled.value = true;
+    }
+}
+
+const handleRemoveTrack = () => {
+    trackFile.value = null;
+    isUploadDisabled.value = true;
+    document.getElementById('track-file').value = null;
+}
 </script>
 
 <template>
     <h2>Add Track</h2>
     <label for="method-selector">Choose a method to add a track </label>
-    <select id="method-selector">
+    <select id="method-selector" @change="handleMethodChange">
         <option value="upload">Upload</option>
         <option value="link">Link</option>
     </select>
-    <input type="file" id="track-file" />
-    <button disabled>Upload</button>
+    <input type="file" id="track-file" @change="handleFileChange" />
+    <button :disabled="isUploadDisabled">Upload</button>
+    <button @click="handleRemoveTrack">Supprimer le morceau</button>
 </template>
 
 <style scoped>
